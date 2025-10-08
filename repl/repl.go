af68ae839367fecfbab86fb834c5bf057f1e8978
obsidian33 <marcus.yu@gmail.com>
2025-10-07 21:36:03 -0700

    3.5 - Evaluating Expressions

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/obsidian33/monkey-lang/evaluator"
 	"github.com/obsidian33/monkey-lang/lexer"
 	"github.com/obsidian33/monkey-lang/parser"
 )
@@ -43,8 +44,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
