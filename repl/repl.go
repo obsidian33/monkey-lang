715259310d13aa6b0e07ffe5fd299e2d69c3248f
obsidian33 <marcus.yu@gmail.com>
2025-10-08 20:39:54 -0700

    3.9 - Bindings & The Environment

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/obsidian33/monkey-lang/evaluator"
 	"github.com/obsidian33/monkey-lang/lexer"
+	"github.com/obsidian33/monkey-lang/object"
 	"github.com/obsidian33/monkey-lang/parser"
 )
 
@@ -26,6 +27,7 @@ const MONKEY_FACE = `            __,__
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -44,7 +46,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
