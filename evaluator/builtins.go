c5a96731a222f9cca2b1f4aeef7f653a1c54f4ac
obsidian33 <marcus.yu@gmail.com>
2025-10-10 23:22:30 -0700

    4.6 - The Grand Finale

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "github.com/obsidian33/monkey-lang/object"
+import (
+	"fmt"
+
+	"github.com/obsidian33/monkey-lang/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -101,6 +105,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
